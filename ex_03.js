d53267c2b30748be763d3c2c8b0326168c948971
ayoub0907 <91942518+ayoub0907@users.noreply.github.com>
2021-10-08 19:37:00 +0200

    Create ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,17 @@
+function changebackground() {
+  var magicNumber = 1;
+  var magicNumber1 = 2;
+  var magicNumber2 = 3;
+
+  if (magicNumber === 1) {
+    paintYellowRectangle();
+  }
+  if (magicNumber1 === 2) {
+    paintGreenRectangle();
+  }
+  if (magicNumber2 === 3) {
+    paintRedRectangle();
+  }
+}
+
+changebackground();
